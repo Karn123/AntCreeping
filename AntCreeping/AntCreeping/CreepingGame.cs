6e7b21cc17ea18946c624c68483f875140c7fd31
Karn <1906817459@qq.com>
2016-09-29 14:17:25 +0800

    commit

--- AntCreeping/AntCreeping/CreepingGame.cs
+++ AntCreeping/AntCreeping/CreepingGame.cs
@@ -1,29 +1,26 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace AntCreeping
 {
     class CreepingGame
     {
-        private int currentTime;
-        private bool isGameOver;
-        #region setters&getters
-        public int CurrentTime
+        private bool isGameOver=false;
+        private List<Ant> antList;
+        private Stick antStick;
+        private Hashtable antHt = new Hashtable();
+        private Timer aTimer;
+        public CreepingGame()
         {
-            get
-            {
-                return currentTime;
-            }
-
-            set
-            {
-                currentTime = value;
-            }
+            AntList = new List<Ant>();
+            AntStick = new Stick();
         }
-
+        #region setters&getters
         public bool IsGameOver
         {
             get
@@ -36,29 +33,112 @@ namespace AntCreeping
                 isGameOver = value;
             }
         }
+
+        internal List<Ant> AntList
+        {
+            get
+            {
+                return antList;
+            }
+
+            set
+            {
+                antList = value;
+            }
+        }
+
+        internal Stick AntStick
+        {
+            get
+            {
+                return antStick;
+            }
+
+            set
+            {
+                antStick = value;
+            }
+        }
         #endregion
 
-        public void setStick()
+        public void removeAnt(Ant ant)
         {
-
+            AntList.Remove(ant);
+            if (AntList.Count == 0)
+                isGameOver = true;
         }
 
-        public void addAntOnTheStick()
+        public bool isHavingCollision(Ant ant)
         {
-
+            return (antHt.ContainsKey(ant.Position));
         }
-
-        public void removeAnt()
-        { }
-
-        public void applyCollisionRule()
-        { }
-
         public void drivingGame()
-        { }
+        {
+            if (!isGameOver)
+            {
+                antHt.Clear();
+                foreach (Ant ant in AntList)
+                {
+                    ant.creeping(1);
+                    if (AntStick.isOutOfRange(ant.Position))
+                    {
+                        removeAnt(ant);
+                        continue;
+                    }
+                    int pos = ant.Position;
+                    if(isHavingCollision(ant))
+                    {
+                        Ant collisionAnt = (Ant)antHt[ant.Position];
+                        if (collisionAnt.CreepingToward == ant.CreepingToward)
+                        {
+                            if (collisionAnt.Velocity > ant.Velocity)
+                            {
+                                collisionAnt.changeCreepDirection();
+                                int i = antList.FindIndex( x => x.Equals((Ant)antHt[ant.Position]));
+                                antList[i] = collisionAnt;
+                            }
+                            else
+                                ant.changeCreepDirection();
+                        }
+                        else
+                        {
+                            collisionAnt.changeCreepDirection();
+                            int i = antList.FindIndex(x => x.Equals((Ant)antHt[ant.Position]));
+                            antList[i] = collisionAnt;
+                            ant.changeCreepDirection();
+                        }
+                    }
+                    else
+                        antHt.Add(pos, ant);
+                }
+                MainPicture.GetInstance().Clear();
+                MainPicture.GetInstance().DrawStick(AntStick.Length);
+                for (int i = 0; i < antList.Count; i++)
+                {
+                    MainPicture.GetInstance().DrawAnt(antList[i].Position,antList[i].CreepingToward);
+                }
+            }
+        }
 
-        public void playGame()
-        { }
-        
+        public void playGame(List<Ant> myAntList,Stick myStick)
+        {
+            AntList = myAntList;
+            AntStick = myStick;
+
+            #region 定时器事件 
+            aTimer = new Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(TimedEvent);
+            aTimer.Interval = 1 * 1000;    //配置文件中配置的秒数
+            aTimer.Enabled = true;
+            #endregion
+        }
+
+        private void TimedEvent(object sender, ElapsedEventArgs e)
+        {
+            if (isGameOver)
+                aTimer.Enabled = false;
+            else
+                drivingGame();
+        }
     }
 } 
--- AntCreeping/AntCreeping/PlayRoom.cs
+++ AntCreeping/AntCreeping/PlayRoom.cs
@@ -19,7 +19,9 @@ namespace AntCreeping
         public const int MAXLENGTH = 300;
         public Toward toward;
         public CreepingGame creeepingGame;
+        public List<Ant> antList;
         public Hashtable ht;
+        public Stick myStick = new Stick();
         public int pos_1 = 30;
         public int pos_2 = 80;
         public int pos_3 = 110;
@@ -29,7 +31,9 @@ namespace AntCreeping
 
         public PlayRoom()
         {
+            myStick.Length = MAXLENGTH;
             creeepingGame = new CreepingGame();
+            antList = new List<Ant>();
             ht = new Hashtable();
         }
 
@@ -43,16 +47,57 @@ namespace AntCreeping
 
         public void addAnt(int pos, Toward dir)
         {
-            MainPicture.GetInstance().DrawAnt(pos, dir);
+            if (ht.ContainsKey(pos))
+            {
+                Ant toBeRemovedAnt = (Ant)ht[pos];
+                antList.Remove(toBeRemovedAnt);
+                ht.Remove(pos);
+            }
             Ant ant = new Ant(pos, dir, default_velocity);
             ht.Add(pos, ant);
+            antList.Add(ant);
         }
+        public void drawFiveAnts()
+        {
+            int pos = 0;
+            Toward dir = Toward.left;
+            //draw five ants
+            for (int i = 1; i <= 5; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        pos = pos_1;
+                        break;
+                    case 2:
+                        pos = pos_2;
+                        break;
+                    case 3:
+                        pos = pos_3;
+                        break;
+                    case 4:
+                        pos = pos_4;
+                        break;
+                    case 5:
+                        pos = pos_5;
+                        break;
+                    default:
+                        break;
+                }
+                MainPicture.GetInstance().DrawAnt(pos, dir);
+            }
+        }
+        /// <summary>
+        /// play all possible games
+        /// </summary>
         public void PlayGames()
         {
             Random rd = new Random();
 
             MainPicture.GetInstance().Clear();
             MainPicture.GetInstance().DrawStick(MAXLENGTH);
+            drawFiveAnts();
+            //set each ant's direction
             for (int i = 0; i < 2; i++)
             {
                 toward = setDirection(i);
@@ -65,7 +110,7 @@ namespace AntCreeping
                     {
                         toward = setDirection(k);
                         addAnt(pos_3, toward);
-                        for(int l=0;l<2;l++)
+                        for(int l = 0;l < 2;l++)
                         {
                             toward = setDirection(l);
                             addAnt(pos_4, toward);
@@ -73,6 +118,7 @@ namespace AntCreeping
                             {
                                 toward = setDirection(m);
                                 addAnt(pos_5, toward);
+                                creeepingGame.playGame(antList,myStick);
                             }
                         }
                     }
